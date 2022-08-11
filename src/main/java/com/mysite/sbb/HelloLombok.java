18fb86bcc7c6bcde4b30b23d8c5a0a1085afbfda
Soyun Won <soyun9902@ewhain.net>
2022-08-11 23:14:29 +0900

    1-05: RequiredArgsConstructor

--- src/main/java/com/mysite/sbb/HelloLombok.java
+++ src/main/java/com/mysite/sbb/HelloLombok.java
@@ -1,18 +1,16 @@
 package com.mysite.sbb;
 
 import lombok.Getter;
-import lombok.Setter;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @Getter
-@Setter
 public class HelloLombok {
-    private String hello;
-    private int lombok;
+    private final String hello;
+    private final int lombok;
 
     public static void main(String[] args) {
-        HelloLombok helloLombok = new HelloLombok();
-        helloLombok.setHello("hello");
-        helloLombok.setLombok(2);
+        HelloLombok helloLombok = new HelloLombok("hello",2); // 생성자 자동 생성
 
         System.out.println(helloLombok.getHello());
         System.out.println(helloLombok.getLombok());
