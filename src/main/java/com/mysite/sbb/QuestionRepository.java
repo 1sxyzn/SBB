068226a4909574e184ff10086721bf7d22b73858
Soyun Won <soyun9902@ewhain.net>
2022-08-16 03:13:05 +0900

    2-05: Repository 데이터 저장하기

---
+++ src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -0,0 +1,7 @@
+package com.mysite.sbb;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository <Question, Integer> { //엔티티 타입과 엔티티 pk의 속성 타입
+
+}
