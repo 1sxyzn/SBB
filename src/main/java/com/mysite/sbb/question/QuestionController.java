2f5b79e56c4565e078419aab2487d098965d72e0
Soyun Won <soyun9902@ewhain.net>
2022-08-18 23:47:34 +0900

    02-7: 템플릿에 데이터 전달 : 질문 목록

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,13 +1,29 @@
 package com.mysite.sbb.question;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
+// 스프링의 의존성 주입(Dependency Injection) 방식 3가지
+// @Autowired 속성 : 속성에 @Autowired 애너테이션을 적용하여 객체를 주입
+// 생성자 : 생성자를 작성하여 객체를 주입 (권장)
+// Setter : Setter 메서드를 작성하여 객체를 주입 (메서드에 @Autowired 애너테이션 필요)
+
+
+@RequiredArgsConstructor // final이 붙은 속성을 포함하는 생성자를 자동으로 생성
 @Controller
 public class QuestionController {
+
+    private final QuestionRepository questionRepository;
+
     @RequestMapping("/question/list")
-    public String list(){
+    public String list(Model model){
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList", questionList); // model : 자바 클래스와 템플릿 간의 연결고리 역할, 템플릿에서 이 값 사용
         return "question_list";
     }
 }
