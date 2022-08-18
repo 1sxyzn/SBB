857409772028e784f4df117597315f941dafca52
Soyun Won <soyun9902@ewhain.net>
2022-08-18 23:50:49 +0900

    02-8: ROOT URL

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -11,4 +11,11 @@ public class MainController {
     public String index(){
         return "welcome to SBB!";
     }
+
+    @RequestMapping("/")
+    public String root() {
+        return "redirect:/question/list";
+    }
+    // redirect:<URL> -> URL로 리다이렉트 (완전히 새로운 URL로 요청)
+    // forward:<URL> -> URL로 포워드 (기존 요청 값들이 유지된 상태로 URL 전환)
 }
