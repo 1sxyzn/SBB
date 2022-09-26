0d01753193bb7a37e76b3329654ab01841fdd22d
Soyun Won <soyun9902@ewhain.net>
2022-09-27 00:35:18 +0900

    3-07: 로그아웃 구현하기

--- src/main/java/com/mysite/sbb/SecurityConfig.java
+++ src/main/java/com/mysite/sbb/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @RequiredArgsConstructor
 @Configuration
@@ -24,7 +25,9 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/**").permitAll().and().csrf().ignoringAntMatchers("/h2-console/**") // 모든 인증되지 않은 요청을 허락
                 .and().headers().addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
-                .and().formLogin().loginPage("/user/login").defaultSuccessUrl("/"); // "/user/login" 으로 로그인하고 성공시 "/"로 이동
+                .and().formLogin().loginPage("/user/login").defaultSuccessUrl("/") // "/user/login" 으로 로그인하고 성공시 "/"로 이동
+                .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/user/logout")).logoutSuccessUrl("/").invalidateHttpSession(true)
+                ;
         // H2는 CSRF 토큰을 발행하는 기능이 없음. 따라서 H2에 대해서만 CSRF 처리를 하지 않도록 예외처리 함
         // H2는 frame 구조로 작성되었고, 스프링 시큐리티는 사이트의 콘텐츠가 다른 사이트에 포함되지 않도록 한다.
         // 따라서 오류가 발생하고 X-Frame-Options 헤더값을 sameorigin으로 설정하여 오류를 해결
