fd24b8f3c6eef2bb961a63c8cd8d8801a612005d
Soyun Won <soyun9902@ewhain.net>
2022-10-02 14:57:18 +0900

    3-13: 마크다운 적용

---
+++ src/main/java/com/mysite/sbb/CommonUtil.java
@@ -0,0 +1,17 @@
+package com.mysite.sbb;
+
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+import org.springframework.stereotype.Component;
+
+
+@Component // 빈으로 등록 -> 템플릿에서 바로 사용 가능
+public class CommonUtil {
+    public String markdown(String markdown){
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse(markdown);
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        return renderer.render(document);
+    }
+}
